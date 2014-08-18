0a0fcb7e089f04d964c14a2f3cebde3e27309640
Ryan Weaver <ryan@thatsquality.com>
2014-08-18 08:34:14 -0400

    [tuts-apply-diff] Applying existing diff file for step config-use-minkcontext-in-behat-yml-suite

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,11 +3,12 @@
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Behat context class.
  */
-class FeatureContext implements SnippetAcceptingContext
+class FeatureContext extends MinkContext implements SnippetAcceptingContext
 {
     private $output;
 
